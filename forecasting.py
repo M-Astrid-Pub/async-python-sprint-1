724d946f75d04404069c10fa22f337e8996e830d
eshumakova <evgeniya.shumakova@dellin.ru>
2023-08-12 16:04:21 +0300

    linters set up and applied

--- forecasting.py
+++ forecasting.py
@@ -2,7 +2,6 @@ import logging
 from concurrent.futures import ThreadPoolExecutor
 from multiprocessing import Queue, Manager, Event
 import os
-from datetime import datetime as dt
 
 from tasks import (
     DataFetchingTask,
@@ -12,18 +11,19 @@ from tasks import (
 )
 from utils import CITIES
 
-file_log = logging.FileHandler('Log.log')
+file_log = logging.FileHandler("Log.log")
 console_out = logging.StreamHandler()
 
-logging.basicConfig(handlers=(file_log, console_out),
-                    format='[%(asctime)s | %(levelname)s]: %(message)s',
-                    datefmt='%m.%d.%Y %H:%M:%S',
-                    level=logging.INFO)
+logging.basicConfig(
+    handlers=(file_log, console_out),
+    format="[%(asctime)s | %(levelname)s]: %(message)s",
+    datefmt="%m.%d.%Y %H:%M:%S",
+    level=logging.INFO,
+)
 
 
-TMP_DIR = 'tmp'
-RES_DIR = 'tmp/result'
-CITIES_DATA = {}
+TMP_DIR = "tmp"
+RES_DIR = "tmp/result"
 
 
 def forecast_weather():
@@ -37,7 +37,7 @@ def forecast_weather():
     queue = Queue()
     load_complete_event = Event()
 
-    logging.info(f'Запускаем worker-процессы для рассчетов средних значений.')
+    logging.info("Запускаем worker-процессы для рассчетов средних значений.")
     calc_tasks = []
     calc_paths = Manager().dict()
     for _ in range(os.cpu_count()):
@@ -45,21 +45,22 @@ def forecast_weather():
         task.start()
         calc_tasks.append(task)
 
-    logging.info(f'Создали пул потоков  для скачивания данных.')
+    logging.info("Создали пул потоков  для скачивания данных.")
     with ThreadPoolExecutor() as pool:
         for city_name in CITIES:
             o_path = os.path.join(TMP_DIR, f"{city_name}.json")
             pool.submit(DataFetchingTask(queue).run, city_name, o_path)
 
-
-    logging.info(f'Закончили скачивание данных. Завершаем процессы рассчета средних.')
+    logging.info(
+        "Закончили скачивание данных. Завершаем процессы рассчета средних."
+    )
     load_complete_event.set()
 
     for task in calc_tasks:
         task.join()
 
     result = []
-    logging.info(f'Запускаем пул потоков для аггрегации данных')
+    logging.info("Запускаем пул потоков для аггрегации данных")
     with ThreadPoolExecutor() as pool:
         pool.map(DataAggregationTask(result).run, calc_paths.values())
 
--- tasks.py
+++ tasks.py
@@ -2,12 +2,12 @@ import json
 import logging
 import os
 import traceback
-from multiprocessing import Queue, Process, Event
+from multiprocessing import Queue, Process
+from multiprocessing.synchronize import Event
 from queue import Empty
 from typing import List, Dict
 from statistics import mean
 from datetime import datetime as dt
-import time
 
 from external import analyzer
 from external.client import YandexWeatherAPI
@@ -25,36 +25,35 @@ def log_exc(func):
 
 
 class DataFetchingTask:
-
     def __init__(self, queue: Queue):
         super().__init__()
         self.queue = queue
 
     @log_exc
     def run(self, city_name: str, out_path: str):
-        logging.info(f'Получаем данные для {city_name}')
+        logging.info(f"Получаем данные для {city_name}")
         url_with_data = get_url_by_city_name(city_name)
         res = YandexWeatherAPI.get_forecasting(url_with_data)
-        with open(out_path, 'w') as fd:
+        with open(out_path, "w") as fd:
             json.dump(res, fd)
-        logging.info(f'Успешно выгрузили данные для {city_name} в {out_path}')
+        logging.info(f"Успешно выгрузили данные для {city_name} в {out_path}")
 
         self.queue.put((city_name, out_path))
-        logging.info(f'Добавили в очередь обработки {city_name}')
+        logging.info(f"Добавили в очередь обработки {city_name}")
 
 
 class DataCalculationTask(Process):
-    def __init__(self, q: Queue, l: Dict, e: Event):
+    def __init__(self, queue: Queue, paths: Dict, event: Event):
         super().__init__()
-        self.queue = q
-        self.paths = l
-        self.break_event = e
+        self.queue = queue
+        self.paths = paths
+        self.break_event = event
 
     @log_exc
     def run(self):
         while True:
             if self.queue.empty() and self.break_event.is_set():
-                logging.info(f'Все задания обработаны. Завершаем {self.name}.')
+                logging.info(f"Все задания обработаны. Завершаем {self.name}.")
                 break
             else:
                 try:
@@ -64,19 +63,23 @@ class DataCalculationTask(Process):
                 self.analyze(city_name, path)
 
     def analyze(self, city_name: str, path: str):
-        logging.info(f'Считаем средние для {path} в {self.name}')
+        logging.info(f"Считаем средние для {path} в {self.name}")
 
         data = analyzer.load_data(path)
         data = analyzer.analyze_json(data)
         mean_tmp, mean_cond = self._count_means(data)
-        data = {'city_name': city_name, 'mean_temperature': mean_tmp, 'mean_condition_hours': mean_cond}
+        data = {
+            "city_name": city_name,
+            "mean_temperature": mean_tmp,
+            "mean_condition_hours": mean_cond,
+        }
 
         out_path = f"tmp/{city_name}_calc.json"
-        with open(out_path, 'w') as fd:
+        with open(out_path, "w") as fd:
             json.dump(data, fd)
 
         self.paths[city_name] = out_path
-        logging.info(f'Успешно посчитали средние для {path} в {self.name}')
+        logging.info(f"Успешно посчитали средние для {path} в {self.name}")
 
     @staticmethod
     def _count_means(data: Dict):
@@ -87,32 +90,30 @@ class DataCalculationTask(Process):
             temps.append(day["temp_avg"])
             conds.append(day["relevant_cond_hours"])
         if not len(temps):
-            raise Exception('No temp data')
+            raise Exception("No temp data")
         return mean(temps), mean(conds)
 
 
 class DataAggregationTask:
-
     def __init__(self, res: List):
         super().__init__()
         self.res = res
 
     @log_exc
     def run(self, path: str):
-        logging.info(f'Выбираем данные из {path}')
+        logging.info(f"Выбираем данные из {path}")
         data = analyzer.load_data(path)
         self.res.append(data)
 
 
 class DataAnalyzingTask:
-
     @log_exc
-    def run(self, data, res_dir):
-        logging.info(f'Сортируем данные для получения рейтинга.')
+    def run(self, data: List, res_dir: str):
+        logging.info("Сортируем данные для получения рейтинга.")
         logging.debug(data)
         self._sort_data(data)
 
-        logging.info(f'Присваиваем рейтинг городам')
+        logging.info("Присваиваем рейтинг городам")
         for rating, city_data in enumerate(data):
             city_data["rating"] = rating + 1
 
@@ -121,12 +122,14 @@ class DataAnalyzingTask:
 
         if not os.path.exists(res_dir):
             os.makedirs(res_dir)
-        res_path = os.path.join(res_dir, f'{dt.now().strftime("%Y.%m.%d.%H%M%S")}.json')
-        with open(res_path, 'w') as fd:
+        res_path = os.path.join(
+            res_dir, f'{dt.now().strftime("%Y.%m.%d.%H%M%S")}.json'
+        )
+        with open(res_path, "w") as fd:
             json.dump(data, fd)
         print(f"Полный результат сохранен в {res_path}")
 
     @staticmethod
     def _sort_data(data):
-        data.sort(key=lambda x: x['mean_condition_hours'])
-        data.sort(key=lambda x: x['mean_temperature'] * -1)
+        data.sort(key=lambda x: x["mean_condition_hours"])
+        data.sort(key=lambda x: x["mean_temperature"] * -1)
